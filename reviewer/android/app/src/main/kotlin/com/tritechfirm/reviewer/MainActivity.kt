9b2e803ffa48bd0c6e53e43c0c22f4ba85b3b9c5
yamenemon <ios.emon@gmail.com>
2020-09-07 23:37:24 +0600

    DELETED SAME REVIEWER PROJECT FROM ROOT DIRECTORY WHICH IS DUPLICATE.

--- reviewer/android/app/src/main/kotlin/com/tritechfirm/reviewer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tritechfirm.reviewer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- reviewer/lib/constants.dart
+++
@@ -1,5 +0,0 @@
-library constants;
-
-import 'package:flutter/material.dart';
-
-const int listCellWidthFactor = 90;
